a7c3f1e1af14711991d03e0ee279fd7163bf8b9a
RICKO CAESAR APRILLA TIAKA <2011102441099@umkt.ac.id>
2024-01-13 01:05:44 +0800

    Fix

--- src/app/_app.jsx
+++ src/app/_app.jsx
@@ -3,9 +3,15 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import NextNProgress from 'nextjs-progressbar';
 config.autoAddCss = false
 library.add(fas, fab)
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <NextNProgress />
+            <Component {...pageProps} />
+        </>
+    )
 }
