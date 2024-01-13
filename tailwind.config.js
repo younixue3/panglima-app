de8736c40e29f79a7c8c9786ed68a5156b3186df
RICKO CAESAR APRILLA TIAKA <2011102441099@umkt.ac.id>
2024-01-13 16:49:00 +0800

    Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './form/**/*.{js,ts,jsx,tsx,mdx}',
+
   ],
   theme: {
     extend: {
